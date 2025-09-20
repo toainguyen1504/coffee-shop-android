d8d76f7b57f5a71c78479c996d18b350aa5de363
toainguyen1504 <toaincgcss200874@fpt.edu.vn>
2025-09-20 10:28:33 +0700

    create splash activity ui

---
+++ app/src/main/java/com/example/coffee_shop_android/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.coffee_shop_android
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffee_shop_android.databinding.ActivitySplashBinding
+import java.util.zip.Inflater
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
