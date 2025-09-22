15b4b89386ce792f00650ddaa2d0113e0eb58468
toainguyen1504 <toaincgcss200874@fpt.edu.vn>
2025-09-22 09:03:53 +0700

    create cart activity ui

---
+++ app/src/main/java/com/example/coffee_shop_android/Activity/CartActivity.kt
@@ -0,0 +1,21 @@
+package com.example.coffee_shop_android.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffee_shop_android.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cart)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
